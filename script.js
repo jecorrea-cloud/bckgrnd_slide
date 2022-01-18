eb0def2e2af73e2638921bcb0bef6ea827f034fc
joaquin <jecorrea@protonmail.com>
2022-01-18 02:36:28 -0500

    right arrow working

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // Bring down the container and the arrows
 
 const body = document.body;
+// slides is a node list
 const slides = document.querySelectorAll(".slide");
 const leftBtn = document.getElementById("left");
 const rightBtn = document.getElementById("right");
 
 let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+
+    slides[activeSlide].classList.add("active");
+  });
+}
